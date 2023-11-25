e7f12ef7b78f7ec4d1fa7ca411460dcf4e82d0fa
Jennifer Valor <profe.jennifer.valor@gmail.com>
2023-11-25 02:12:20 -0300

    agregamos comentarios y una variable

---
+++ js/1-variables.js
@@ -0,0 +1,22 @@
+// esto es un comentario de una sola linea 
+
+/*
+esto es un 
+comentario de 
+mútiples lineas
++/
+
+// variables (es un espacio de memoria en nuestra pc que va almacenar algo)
+
+/*Inicializar una variable (var, let, const) - crear una variable, la mas usada son let y const. 
+Si necesito cambiar el valor de la variable uso let
+Si el valor no va a cambiar nunca uso const
+Var se utilizaba antes, ahora y no es usual
+*/
+
+/*Al crear una variable debo colocar nombres intuitivos, para saber que estaba guardando alli,
+el signo = es el operador de asignación. Significa que el valor que escriba a la derecha, estará almacenado
+o guardado en la cajita o variable comisión. 
+Para guardar string (cadena de texto) se usa comillas dobles o simples.  
+*/
+let comision = "c74i";
